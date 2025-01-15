1c781f5f7b72a868a02a1cefca04b648efd4e703
MustafaGaber1 <mostafagaber657@gmail.com>
2025-01-15 23:35:43 +0200

    feat: add dark mode

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,14 +1,22 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
+import { ThemeProvider } from "./components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <main>
-        <SidebarTrigger />
-        {children}
-      </main>
-    </SidebarProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <SidebarProvider>
+        <div className="absolute top-2 right-2">
+          <ModeToggle />
+        </div>
+        <AppSidebar />
+
+        <main>
+          <SidebarTrigger />
+          {children}
+        </main>
+      </SidebarProvider>
+    </ThemeProvider>
   );
 }
